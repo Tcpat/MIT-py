4c840bf207e25ba0d75b45def38f8e9ab6bb6dee
Tcpat <66862715+Tcpat@users.noreply.github.com>
2020-06-17 21:06:48 -0400

    Create Fib_dictionary.py

---
+++ Fib_dictionary.py
@@ -0,0 +1,10 @@
+def fib_dict(n, d):
+    if n in d:
+        return d[n]
+    else:
+        ans = fib_dict(n-1, d) + fib_dict(n-2, d)
+        d[n] = ans
+        return ans
+
+d = {1:1, 2:2}
+print(fib_dict(1033, d))
